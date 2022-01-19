6a4f842c2f3c4efdba9a12bdf4885c1442c0de6a
Denis <denis_777@ukr.net>
2022-01-20 00:00:58 +0200

    autofill

--- template-parts/content/search-form-custom.php
+++ template-parts/content/search-form-custom.php
@@ -10,7 +10,7 @@ $twentytwentyone_aria_label = ! empty( $args['aria_label'] ) ? 'aria-label="' . 
 <form id="ajax-serach-form" role="search" <?php echo $twentytwentyone_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form search-form-custom" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label for="<?php echo esc_attr( $custom_id ); ?>"><?php _e( 'Search&hellip;', 'twentytwentyone' ); // phpcs:ignore: WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations ?></label>
     <label for="<?php echo esc_attr( $custom_id ); ?>" id="search-clean" class="search-clean"></label>
-    <input type="search" id="<?php echo esc_attr( $custom_id ); ?>" class="search-field custom-search-field" value="" name="s" />
+    <input type="search" id="<?php echo esc_attr( $custom_id ); ?>" class="search-field custom-search-field" value="" name="s" autocomplete="off" />
     <input type="submit" class="search-submit search-submit-mob" value="<?php echo esc_attr_x( 'Search', 'submit button', 'twentytwentyone' ); ?>" />
 </form>
 <div id="ajax_search_results" class="ajax_search_results search-form">
